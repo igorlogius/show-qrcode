af34dd7d2958610d14731bdc5e1d66b1a9793038
igorlogius <67047467+igorlogius@users.noreply.github.com>
2020-07-14 21:50:28 +0200

    refactor: add some checks

--- popup.js
+++ popup.js
@@ -1,23 +1,23 @@
-
+// get current activ tab
 browser.tabs.query({active: true, currentWindow: true}).then( (tabs) => {
 
-	//console.log(tabs[0].id, tabs[0].url);
-	const tab = tabs[0];
-	const url = tab.url;
+	if ( !Array.isArray(tabs) ) { return; }
+	if ( tabs.length < 1 ) { return; }
+	if ( typeof tabs[0].url !== 'string' ) { return; }
 
-	 var qr = new QRious({
-	  background: "white",
-	  backgroundAlpha: 1.0,
-          element: document.getElementById('qr'),
-	  foreground: "black",
-	  foregroundAlpha: 1.0,
-	  level: "L",
-	  mime: "image/png",
-	  size: window.innerWidth,
-          value: url 
-        });
+	new QRious({
+		background: "white",
+		backgroundAlpha: 1.0,
+		element: document.getElementById('qr'),
+		foreground: "black",
+		foregroundAlpha: 1.0,
+		level: "L",
+		mime: "image/png",
+		size: window.innerWidth,
+		value: tabs[0].url 
+	});
 
 
-} ).catch( (err) => {console.log(err);} );
+});
 
 
