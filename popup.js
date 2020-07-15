35938ac241914bfcae1acae00ef07a35403890c2
igorlogius <67047467+igorlogius@users.noreply.github.com>
2020-07-15 14:37:03 +0200

    use async + await to handle promise

--- popup.js
+++ popup.js
@@ -1,23 +1,31 @@
-// get current activ tab
-browser.tabs.query({active: true, currentWindow: true}).then( (tabs) => {
 
-	if ( !Array.isArray(tabs) ) { return; }
-	if ( tabs.length < 1 ) { return; }
-	if ( typeof tabs[0].url !== 'string' ) { return; }
+async function init() {
 
-	new QRious({
-		background: "white",
-		backgroundAlpha: 1.0,
-		element: document.getElementById('qr'),
-		foreground: "black",
-		foregroundAlpha: 1.0,
-		level: "L",
-		mime: "image/png",
-		size: window.innerWidth,
-		value: tabs[0].url 
-	});
+	try {
+		// get current activ tab
+		const tabs = await browser.tabs.query({active: true, currentWindow: true});
+
+		if (            !Array.isArray(tabs) ) { throw 'tabs query return no array'; }
+		if (                 tabs.length < 1 ) { throw 'tabs length is less than 1'  }
+		if ( typeof tabs[0].url !== 'string' ) { throw 'tab.url is not a string';    }
+
+		new QRious({
+			background: "white",
+			backgroundAlpha: 1.0,
+			element: document.getElementById('qr'),
+			foreground: "black",
+			foregroundAlpha: 1.0,
+			level: "L",
+			mime: "image/png",
+			size: window.innerWidth,
+			value: tabs[0].url 
+		});
+
+	}catch(e){
+		console.error(e);
+	}
+}
 
 
-});
-
+init();
 
