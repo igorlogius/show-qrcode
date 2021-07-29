0219771440c49e7603b78a25d4474258af422f7c
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-07-29 16:39:43 +0200

    feat: add save button feat: add img transparancy feat: make background gray to be more eyefriendly

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 
-async function init() {
+(async function () {
 
 	try {
 		// get current activ tab
@@ -9,23 +9,27 @@ async function init() {
 		if (                 tabs.length < 1 ) { throw 'tabs length is less than 1'  }
 		if ( typeof tabs[0].url !== 'string' ) { throw 'tab.url is not a string';    }
 
-		new QRious({
+		const qr = new QRious({
 			background: "white",
-			backgroundAlpha: 1.0,
-			element: document.getElementById('qr'),
+			backgroundAlpha: 0.0, // make background transparent
 			foreground: "black",
 			foregroundAlpha: 1.0,
-			level: "L",
-			mime: "image/png",
-			size: window.innerWidth,
+			level: "L", // large
+			mime: "image/png", // portable network graphics 
+			size: 940, // max size is size of the window width
 			value: tabs[0].url 
 		});
+
+		const dataurl =qr.toDataURL('image/png');
+		qrimg = document.getElementById('qrimg')
+		qrimg.src=dataurl;
+		qrlink = document.getElementById('qrlink')
+		qrlink.download= "qr_" + encodeURIComponent(tabs[0].url) + ".png"
+		qrlink.href=dataurl;
 
 	}catch(e){
 		console.error(e);
 	}
-}
+}());
 
-
-init();
 
